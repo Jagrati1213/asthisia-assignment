c656d4387409c71394187205253d7fd0628ee23c
jagrati gupta <123jagratigupta@gmail.com>
2024-08-31 13:18:50 +0530

    JG: Updates - Added form data - handle deploy - handle input changes

--- src/components/SetupComponent/ConfigureStep/index.tsx
+++ src/components/SetupComponent/ConfigureStep/index.tsx
@@ -3,8 +3,39 @@ import { Pricing_Data } from "../../../DummyData/constant";
 import InputFieldComponent from "./InputFieldComponent";
 import { PricingCardComponent } from "./PricingCardComponent";
 
+interface FormData {
+  port: number;
+  command: string;
+  arguments: string;
+  envVars: string;
+}
+
 export function ConfigureStep() {
   const [selectedPrice, setSelectedPrice] = useState<string | null>("Micro");
+
+  const [formData, setFormData] = useState<FormData>({
+    port: 8088,
+    command: "My-deployment",
+    arguments: "My-deployment",
+    envVars: "ENV_NAME=ENV_VALUE",
+  });
+
+  const handleInputChange = (field: keyof FormData, value: string) => {
+    console.log(field, value);
+    setFormData((prevState) => ({
+      ...prevState,
+      [field]: value,
+    }));
+  };
+
+  const handleDeploy = () => {
+    const { port, command, arguments: args, envVars } = formData;
+    if (!port || !command || !args || !envVars || !selectedPrice) {
+      alert("Please fill all input fields and select a pricing option.");
+    } else {
+      alert("Successfully deploy!");
+    }
+  };
 
   return (
     <div className="border border-asBorderGray p-8 rounded-lg font-Inter">
@@ -19,16 +50,25 @@ export function ConfigureStep() {
       <div className="flex flex-col mt-6 gap-8">
         <InputFieldComponent
           fieldName="Port"
-          fieldValue="8088"
+          fieldValue={formData.port}
           fieldType="number"
+          onChange={(value) => handleInputChange("port", value)}
         />
-        <InputFieldComponent fieldName="Command" fieldValue="My-deployment" />
-        <InputFieldComponent fieldName="Arguments" fieldValue="My-deployment" />
+        <InputFieldComponent
+          fieldName="Command"
+          fieldValue={formData.command}
+          onChange={(value) => handleInputChange("command", value)}
+        />
+        <InputFieldComponent
+          fieldName="Arguments"
+          fieldValue={formData.arguments}
+          onChange={(value) => handleInputChange("arguments", value)}
+        />
         <InputFieldComponent
           fieldName="Environment Variables"
-          fieldValue="ENV_NAME=ENV_VALUE"
+          fieldValue={formData.envVars}
+          onChange={(value) => handleInputChange("envVars", value)}
         />
-
         <div className="flex flex-col gap-2">
           <p className="font-Inter text-sm font-normal text-asLightGray">
             Pricing Configuration
@@ -51,7 +91,10 @@ export function ConfigureStep() {
         </div>
 
         <div className="p-[1px] bg-gradient-to-r from-[#1A73E8] via-[#853BCE] to-[#C471ED] rounded-md">
-          <button className="w-full z-10 bg-asBlack rounded-md py-3">
+          <button
+            className="w-full z-10 bg-asBlack rounded-md py-3"
+            onClick={handleDeploy}
+          >
             Deploy
           </button>
         </div>
