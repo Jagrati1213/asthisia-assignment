6d2ba1261ed3b5cab9d8a0c4f275c294d9c3cfbb
jagrati gupta <123jagratigupta@gmail.com>
2024-08-31 13:17:27 +0530

    JG: added scroll

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,20 @@ import { WelcomeHeader } from "./components/WelcomeHeader/index.js";
 
 function App() {
   return (
-    <div className="flex md:max-w-7xl max-w-full md:mx-auto mx-0 bg-asBlack text-white min-h-screen h-full">
+    <div className="flex md:max-w-7xl max-h-screen max-w-full md:mx-auto mx-0 bg-asBlack text-white min-h-screen h-full overflow-hidden">
       <Navigation />
       <div className="w-full">
         <WelcomeHeader userName="Jagrati" />
-        <SetupComponent />
+        <div
+          className="h-[calc(100vh-80px)] overflow-auto "
+          style={{
+            /* Hide scrollbar for Chrome, Safari, and Opera */
+            scrollbarWidth: "none" /* Firefox */,
+            msOverflowStyle: "none" /* IE and Edge */,
+          }}
+        >
+          <SetupComponent />
+        </div>
       </div>
     </div>
   );
